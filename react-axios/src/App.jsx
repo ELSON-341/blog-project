b7309f007e0c6a04545969c26db815a683a77c63
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-08-06 15:00:31 -0300

    ini

--- react-axios/src/App.jsx
+++ react-axios/src/App.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Rect axios</h1>
+      <Navbar/>
+      <div className="container">
+        <Outlet/>
+      </div>
     </div>
   )
 }
